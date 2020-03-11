9b2a34e78439efb6961b188f49e40f76f8f904f0
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-03-11 17:45:08 +0200

    constructor

--- +inst/@ZWO_ASICamera/ZWO_ASICamera.m
+++ +inst/@ZWO_ASICamera/ZWO_ASICamera.m
@@ -1,9 +1,25 @@
 classdef ZWO_ASICamera < handle
 
-properties
-end
+    properties
+    end
 
-methods
-end
+    methods
+
+       % Constructor
+       function Z=ZWO_ASICamera()
+           if libisloaded('libASICamera2')
+               unloadlibrary('libASICamera2')
+           end
+           classpath=fileparts(mfilename('fullpath'));
+           loadlibrary(fullfile(classpath,'lib/libASICamera2.so'),...
+               fullfile(classpath,'lib/ASICamera2.h'))
+
+       end
+
+       % destructor
+       function delete(Z)
+       end
+
+    end
 
 end
